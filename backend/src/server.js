d2f864960e474642065703d5aed51e4917fc9429
Ankit Kumar <ankitkumarjhajharia001@gmail.com>
2026-07-01 11:16:09 +0530

    backend changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,7 +32,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "https://mediqure.in",
+            "https://www.mediqure.in",
         ],
         credentials: true
     })
